f2c34e146ab0637f9fd7e8739fbe6baa4e0556d6
VEDANTSUNILRAUT <vedantsunilraut@gmail.com>
2024-08-10 10:54:18 +0530

    hamburger sign

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
+  // When we click on each nav__link, we remove the 'show' class
   navMenu.classList.remove("show");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
